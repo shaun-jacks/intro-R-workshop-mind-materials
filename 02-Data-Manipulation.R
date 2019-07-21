04374ba15a1bb56c4158ec9ece0300f67abd6038
Shaun Jackson <sjackson95@gmail.com>
2019-07-21 16:17:50 -0700

    added modifcations after going through materials

--- 02-Data-Manipulation.R
+++ 02-Data-Manipulation.R
@@ -1,85 +1,68 @@
 #-----------------------------------------------------------------------------#
 #### Removing Duplicates ####
 #-----------------------------------------------------------------------------#
-# Create duplicated dataframe for demonstration
-dupDf <- data.frame(id = c(1, 1, 2, 2, 3, 3),
-                    visit = c('10 months', '20 months',  '10 months', 
-                              '10 months', '20 months', '20 months'),
-                    item1 = c(1, 2, 3, 3, 5, 6))
+# Read in dataframe with duplicates
+dup_adosm1 <- read.csv('./datasets/adosm1_duplicates.csv',
+                       stringsAsFactors = FALSE)
+
 # save logical vector of duplicate indices
-dupIndGrouped <- duplicated(dupDf[, c("id", "visit")])
+dupIndGrouped <- duplicated(dup_adosm1[, c("id", "visit")])
 # Remove duplicates
 notDuplicateCases <- !dupIndGrouped
-fixedDf <- dupDf[notDuplicateCases, ]
+fixedDf <- dup_adosm1[notDuplicateCases, ]
 View(fixedDf)  # View fixed dataframe
 
 #-----------------------------------------------------------------------------#
-#### Cleaning Messy Data ####
+#### Finding NAs, and relpacing Coded Values ####
 #-----------------------------------------------------------------------------#
-messyDf <- data.frame(
-  id = c(1, 1, 2, 2, 3, 3),
-  visit = c('10 months', '20 months',  '10 months', 
-            '20 months', '10 months', '20 months'),
-  item1 = c(1, 2, 3, 3, 5, 6),
-  # item2 is a column with NAs
-  item2 = c(NA, NA, 3, 4, 5, 6),
-  # item3 is  a column with multiple missing codes and NAs
-  item3 = c(NA, 'Missing', '-999', '4', '5', '6'),
-  # item 4 is a character column with different codes for  same value
-  item4Text = c('father', 'Father', 'fth', 'mother', 'Mother', 'Mother'),
-  # item 5 is a coded variable with text embedded within
-  item5Codes = c('0 (Never)', '1', '2', '3', '4', '5 (Always)'),
-  item6 = c(1, 2, NA, NA, NA, NA),
-  item7 = c(1, 2, 3, NA, NA, 6)
-)
+# Read in messy dataset that we will clean
+parentQs <- read.csv('./datasets/messyDataset.csv', stringsAsFactors = FALSE)
 
 
 # Finding NAs with is.na()
-is.na(messyDf$item2)
+is.na(parentQs$item2)
 
 ## Using ifelse()
 
 # for all NAs replace with 0, otherwise replace with original value
-ifelse(is.na(messyDf$item2), 0, messyDf$item2)
+ifelse(is.na(parentQs$item2), 0, parentQs$item2)
 
 # We use this vector and assign it to the original column to clean it
-messyDf$item2 <- ifelse(is.na(messyDf$item2), 0, messyDf$item2)
+parentQs$item2 <- ifelse(is.na(parentQs$item2), 0, parentQs$item2)
 
 ## Nesting ifelse() statements
 # ifelse(condition 1, if condition 1 true then do this, else:
 #   ifelse(condition2, if condition2 is true then do this, else: 
-#     do this))
-# See  logical vector of NAs
-is.na(messyDf$item3)
-# See  logical vector of -999s
-messyDf$item3 == '-999'
-# See  logical vector of 'Missing'
-messyDf$item3 == 'Missing'
+#     replace with last value))
 
-# for all NAs or missing codes, replace with -9, otherwise replace with original value
-ifelse(is.na(messyDf$item3), '-9', 
-       ifelse(messyDf$item3 == '-999', '-9',
-              ifelse(messyDf$item3 == 'Missing', '-9', messyDf$item3)))
+# Look at item5Codes
+parentQs$item5Codes
 
-# We use this vector and assign it to the original column to clean it
-messyDf$item3 <- 
-  ifelse(is.na(messyDf$item3), '-9', 
-         ifelse(messyDf$item3 == '-999', '-9',
-                ifelse(messyDf$item3 == 'Missing', '-9', messyDf$item3)))
+# Replace coded values with it's numeric form
+parentQs$item5Codes <- 
+  ifelse(parentQs$item5Codes == "0 (Never)", "0",
+       ifelse(parentQs$item5Codes == "5 (Always)", "5",
+              parentQs$item5Codes))
+# Convert a character vector to a numeric
+parentQs$item5Codes <- as.numeric(parentQs$item5Codes)
+
+#-----------------------------------------------------------------------------#
+#### Assignment ####
+#-----------------------------------------------------------------------------#
+# Using the parentQs created in your R environment, clean up item 4
+# Replace the item 4 values that represent a "father" to "father"
+# and that represent a "mother" to "mother"
+parentQs$item4Text
+
+
 
 
 
 #-----------------------------------------------------------------------------#
 #### Writing Data ####
 #-----------------------------------------------------------------------------#
+# write.csv() is a function you can use to write a dataframe to a CSV file.
 
-write.csv(adosm1, './datasets/ADOS Module 1 Scored', row.names = FALSE)
-
-#-----------------------------------------------------------------------------#
-#### Assignment ####
-#-----------------------------------------------------------------------------#
-
-# 
-# - Using the `messyDf` created in your R environment, clean up items 4 and 5
-# - Replace the item 4 values to a consistent value to represent father and mother
-# - Replace the item 5 value: `0 (Never)` and `5 (Always)` to `0` and `5`
+# Write your cleaner dataset to the datasets folder and name it
+# cleanedDf.csv
+write.csv(parentQs, './datasets/cleanedDf.csv', row.names = FALSE)
--- 04-Data-Analysis.R
+++ 04-Data-Analysis.R
@@ -1,11 +1,13 @@
 ## Load required libraries
 library("lme4")  # For creating mixed effects models
+# install.packages("lme4")
 library("effects")  # For plotting models
+# install.packages("effects")
 
 #-----------------------------------------------------------------------------#
 #### Read in data ####
 #-----------------------------------------------------------------------------#
-adosm2 <- read.csv('./datasets/adosm2_scored.csv', 
+adosm2 <- read.csv("./datasets/adosm2_scored.csv", 
                    stringsAsFactors = FALSE)
 #-----------------------------------------------------------------------------#
 #### Basic Stats functions ####
@@ -17,24 +19,27 @@ median(adosm2$ados_sarb_total, na.rm = TRUE)
 sd(adosm2$ados_sarb_total, na.rm = TRUE)
 summary(adosm2$ados_sarb_total)
 
+
 ## Chi-Squared Test
 chisq.test(table(adosm2$cbe_36, adosm2$recruitment_group))
+
+### Correlation
+
+# Find correlation between two vars
+cor.test(adosm2$ados_fake_score1, adosm2$ados_fake_lin_outcome)
 
 #-----------------------------------------------------------------------------#
 #### Linear Regression ####
 #-----------------------------------------------------------------------------#
 
-### Correlation
-
-# - Find correlation between two vars
-cor.test(adosm2$ados_fake_score1, adosm2$ados_fake_lin_outcome)
 
 ### Creating a Linear Regression Model
 
-# - We use the `lm()` functionn to create a Linear Regression Model
-# - The first parameter takes in an equation of the form: `dependentVar ~ predictor1 + predictor2 + ...`
-# - So the dependentVar is a function of the predictor variables
-# - In order to use column names as variables in this eq., the `data` parameter takes in a dataframe as input.
+# We use the lm() function to create a Linear Regression Model
+# The first parameter takes in an equation of the form: 
+# "dependentVar ~ predictor1 + predictor2 + ..."
+# So the dependentVar is a function of the predictor variables
+# In order to use column names as variables in this eq., the `data` parameter takes in a dataframe as input.
 
 
 # To demonstrate a linear model, we use fake ados columns within the ados
@@ -42,53 +47,65 @@ cor.test(adosm2$ados_fake_score1, adosm2$ados_fake_lin_outcome)
 linearMod <- 
   lm(ados_fake_lin_outcome ~ ados_fake_score1 + ados_fake_score2,
      data=adosm2)
-### Residuals, P-Values, Coefficients, with `summary()`
+
+linearMod
+### Residuals, P-Values, Coefficients, with summary()
 
 summary(linearMod)
 
 
 ### Plotting the Linear Model
 # create effects object
-plot(Effect(c('ados_fake_score1'), linearMod))
-plot(Effect(c('ados_fake_score2'), linearMod))
-plot(Effect(c('ados_fake_score1', 'ados_fake_score2'), linearMod))
+eff_fake_score1 <- Effect(c("ados_fake_score1"), linearMod)
+plot(eff_fake_score1)
+eff_fake_score2 <- Effect(c("ados_fake_score2"), linearMod)
+plot(eff_fake_score2)
+eff_fake_score_1_2<- Effect(c("ados_fake_score1", "ados_fake_score2"), linearMod)
+plot(eff_fake_score_1_2)
+
+
 
 #-----------------------------------------------------------------------------#
 #### Logistic Regression ####
 #-----------------------------------------------------------------------------#
-adosm2$cbe_asd <- ifelse(adosm2$cbe_36 %in% c('Autism', 'ASD'), 'ASD', 'Non-ASD')
+adosm2$cbe_asd <- ifelse(adosm2$cbe_36 %in% c("Autism", "ASD"), "ASD", "Non-ASD")
+
 adosm2$cbe_asd <- as.factor(adosm2$cbe_asd)
-adosm2$cbe_asd <- relevel(adosm2$cbe_asd, ref = 'Non-ASD')
+
+adosm2$cbe_asd <- relevel(adosm2$cbe_asd, ref = "Non-ASD")
+
 mod <- glm(cbe_asd ~ 1 + ados_sarb_total,
            data = adosm2,
-           family = 'binomial')
-eff <- Effect(c('ados_sarb_total'), mod)
+           family = "binomial")
+
+eff <- Effect(c("ados_sarb_total"), mod)
 plot(eff,
      axes = list(
        y = list(
-         lab = 'CBE of ASD',
+         lab = "CBE of ASD",
          ticks = list(at = c(.01, .1, .2, .4, .6, .8,  .99))))
 )
+
+
 
 
 #-----------------------------------------------------------------------------#
 #### Mixed Effects Models ####
 #-----------------------------------------------------------------------------#
-
 ### Mixed Effects Logistic Regression
 
 # analyze- logistic regression mixed effects
-adosm2$cbe_asd <- ifelse(adosm2$cbe_36 %in% c('Autism', 'ASD'), 'ASD', 'Non-ASD')
+adosm2$cbe_asd <- ifelse(adosm2$cbe_36 %in% c("Autism", "ASD"), "ASD", "Non-ASD")
 adosm2$cbe_asd <- as.factor(adosm2$cbe_asd)
-adosm2$cbe_asd <- relevel(adosm2$cbe_asd, ref = 'Non-ASD')
+adosm2$cbe_asd <- relevel(adosm2$cbe_asd, ref = "Non-ASD")
 mod <- glmer(cbe_asd ~ 1 + (1|visit) + ados_sarb_total,
                    data = adosm2,
-                   family = 'binomial')
-eff <- Effect(c('ados_sarb_total'), mod)
+                   family = "binomial")
+eff <- Effect(c("ados_sarb_total"), mod)
 plot(eff,
      axes = list(
        y = list(
-         lab = 'CBE of ASD',
+         lab = "CBE of ASD",
          ticks = list(at = c(.01, .1, .2, .4, .6, .8,  .99))))
 )
 
